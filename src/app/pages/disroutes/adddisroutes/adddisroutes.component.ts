ed10bbaf2b20bb2256a2259271e92e3d3e343e2a
HasithaErandaka <hasiera7@gmail.com>
2021-02-12 12:26:39 +0530

    routes

--- src/app/pages/disroutes/adddisroutes/adddisroutes.component.ts
+++ src/app/pages/disroutes/adddisroutes/adddisroutes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Disroute} from '../../../models/disroutes';
+import {DisroutsService} from '../../../services/disroutes.service';
 
 @Component({
   selector: 'app-adddisroutes',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adddisroutes.component.scss']
 })
 export class AdddisroutesComponent implements OnInit {
+  city="";
+  towns:any[]=[];
 
-  constructor() { }
+  disroute: Disroute={
+    route_name:'',
+    cities:this.towns
+  }
+  constructor(private disrouteService:DisroutsService) { }
 
   ngOnInit(): void {
   }
+
+  addcities(){
+    this.towns.push(this.city);
+    this.city="";
+    this.towns.forEach(function(value){
+      console.log(value);
+    })
+  }
+
+  onSubmit(){
+    if(this.disroute.route_name!=''
+    && this.disroute.cities!=[]){
+      this.disrouteService.addDisroutes(this.disroute);
+      alert('New route has been added successfully');
+      
+    }else{ alert('error adding the route')}
+    
+  }
+
+
 
 }
