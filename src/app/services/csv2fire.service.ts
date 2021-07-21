dae326b17623c30e30802327efc13297fba98d41
LasithaNiroshana <lasithaniroshanawm@gmail.com>
2021-07-21 22:49:30 +0530

    bulk upload

--- src/app/services/csv2fire.service.ts
+++ src/app/services/csv2fire.service.ts
@@ -1,40 +1,43 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import * as Papa from 'papaparse';
-import * as _ from 'lodash';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import * as Papa from "papaparse";
+import * as _ from "lodash";
+import { MatSnackBar } from "@angular/material/snack-bar";
 @Injectable({
-providedIn: 'root'
+  providedIn: "root",
 })
 export class Csv2fireService {
-csv_rec: any[] = [];
-header = false;
-constructor(private afs: AngularFirestore,private snackBar:MatSnackBar) { }
-process(file, collection) {
-Papa.parse(file, {
-complete: res => {
-this.csv_rec = res;
-this.firethis(this.csv_rec['data'], collection);
-},
-header: true
-});
-}
-firethis(json, collection) {
-return new Promise<void>((resolve) => {
-_.map(json, (e) => {
-_.keys(e).map(() => {
-this.afs.collection('orders').add(e);
-this.openSnackBar('List of orders added successfully','');
-})
-})
-resolve();
-})
-}
+  csv_rec: any[] = [];
+  header = false;
 
-openSnackBar(message: string, action: string) {
-  this.snackBar.open(message, action, {
-    duration: 3200,
-  });
-}
+  constructor(private afs: AngularFirestore, private snackBar: MatSnackBar) {}
 
+  process(file, collection) {
+    Papa.parse(file, {
+      complete: (res) => {
+        this.csv_rec = res;
+        this.firethis(this.csv_rec["data"], collection);
+      },
+      header: true,
+    });
+  }
+  firethis(json, collection) {
+    return new Promise<void>((resolve) => {
+      _.map(json, (e, i) => {
+        _.keys(e).map(() => {
+          this.afs
+            .collection("orders")
+            .add(e);
+          this.openSnackBar("List of orders added successfully", "");
+        });
+      });
+      resolve();
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 3200,
+    });
+  }
 }
--- src/app/services/cus-upload.service.ts
+++ src/app/services/cus-upload.service.ts
@@ -6,38 +6,35 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 @Injectable({
 providedIn: 'root'
 })
-@Injectable({
-  providedIn: 'root'
+export class Csv2fireService {
+csv_rec: any[] = [];
+header = false;
+constructor(private afs: AngularFirestore,private snackBar:MatSnackBar) { }
+process(file, collection) {
+Papa.parse(file, {
+complete: res => {
+this.csv_rec = res;
+this.firethis(this.csv_rec['data'], collection);
+},
+header: true
+});
+}
+firethis(json, collection) {
+return new Promise<void>((resolve) => {
+_.map(json, (e, i) => {
+_.keys(e).map(() => {
+this.afs.collection('orders').doc('doc ' + i).set(e);
+this.openSnackBar('List of orders added successfully','')
 })
-export class CusUploadService {
-  csv_rec: any[] = [];
-  header = false;
-  constructor(private afs: AngularFirestore,private snackBar:MatSnackBar) { }
-  process(file, collection) {
-  Papa.parse(file, {
-  complete: res => {
-  this.csv_rec = res;
-  this.firethis(this.csv_rec['data'], collection);
-  },
-  header: true
+})
+resolve();
+})
+}
+
+openSnackBar(message: string, action: string) {
+  this.snackBar.open(message, action, {
+    duration: 3200,
   });
-  }
-  firethis(json, collection) {
-  return new Promise<void>((resolve) => {
-  _.map(json, (e) => {
-  _.keys(e).map(() => {
-  this.afs.collection('customers').add(e);
-  this.openSnackBar('List of cutomers added successfully','')
-  })
-  })
-  resolve();
-  })
-  }
+}
 
-  openSnackBar(message: string, action: string) {
-    this.snackBar.open(message, action, {
-      duration: 3200,
-    });
-  }
-
-  }
+}
